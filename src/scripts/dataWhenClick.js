fc3b5c4ec8eaa411882fba2a2454fb3cf9abdf80
osapkal <osapkal@asu.edu>
2026-04-03 10:45:59 -0700

    updates

--- src/scripts/dataWhenClick.js
+++ src/scripts/dataWhenClick.js
@@ -1,32 +1,24 @@
 async function buttonLog(id, value, buttonPressed) {
-    // alert(id)
-    date = new Date()
+    date = new Date();
 
     $.ajax({
         async: false,
         type: "POST",
         url: "saveData.php",
         data: { "name": id, "date": date },
-        //dataType: "json",
-        success: function(msg) {
-            // alert("success")
-            // alert(msg)
+        complete: function () {
             window.location.href = value;
-
         },
-        error: function(msg) {
-            alert("error");
+        error: function () {
+            // POST fails when PHP/MySQL is not available (e.g. static python server); navigation still happens in complete.
         },
-
-
     });
 
-
-    url = 'http://WIZnetD9E9F9.istb4.dhcp.asu.edu/' + buttonPressed;
+    var url = "http://WIZnetD9E9F9.istb4.dhcp.asu.edu/" + buttonPressed;
     console.log(url);
-    fetch(url, { method: 'POST', mode: 'no-cors' })
-        .then(data => console.log(data));
+    fetch(url, { method: "POST", mode: "no-cors" }).then(function (data) {
+        console.log(data);
+    });
 
     return true;
-
 }
